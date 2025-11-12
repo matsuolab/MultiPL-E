a3195258c7eb43f5e29b2e96db8d61c139bef18a
Ikuya Yamada <ikuya@ikuya.net>
2025-11-12 15:49:19 +0900

    Add --trust-remote-code option to dataset_diff.py

--- dataset_builder/dataset_diff.py
+++ dataset_builder/dataset_diff.py
@@ -1,39 +1,37 @@
-# /// script
-# requires-python = ">=3.12"
-# dependencies = [
-#     "datasets",
-# ]
-# ///
 """
 This script summarizes the delta between two revisions of MultiPL-E.
 """
+import datasets
 import argparse
+import pandas as pd
+from typing import List
+from tqdm.auto import tqdm
 import difflib
 from pathlib import Path
-from typing import List
-
-import pandas as pd
-from tqdm.auto import tqdm
-
-import datasets
 
 # cspell:ignore nuprl tqdm dataframes iloc
-
 
 def simple_diff(txt1: str, txt2: str):
     differ = difflib.Differ()
     return "\n".join(differ.compare(txt1.splitlines(), txt2.splitlines()))
 
-
 def find_common_configs_and_report_added_and_removed(
-    path: str, old_revision: str, new_revision: str
+    path: str, trust_remote_code, old_revision: str, new_revision: str
 ) -> List[str]:
     """
     Returns the list of common configs. Prints the configs added and removed
     between the two revisions.
     """
-    old_configs = set(datasets.get_dataset_config_names(path, revision=old_revision))
-    new_configs = set(datasets.get_dataset_config_names(path, revision=new_revision))
+    old_configs = set(
+        datasets.get_dataset_config_names(
+            path, revision=old_revision, trust_remote_code=trust_remote_code
+        )
+    )
+    new_configs = set(
+        datasets.get_dataset_config_names(
+            path, revision=new_revision, trust_remote_code=trust_remote_code
+        )
+    )
 
     removed_configs = list(old_configs - new_configs)
     removed_configs.sort()
@@ -54,39 +52,33 @@ def find_common_configs_and_report_added_and_removed(
     return common_splits
 
 
-def compare_configs_revisions(
-    path: str, config: str, old_revision: str, new_revision: str
-):
+def compare_configs_revisions(path: str, config: str, old_revision: str, new_revision: str, trust_remote_code: bool):
     old_df = datasets.load_dataset(
-        path, config, split="test", revision=old_revision
+        path, config, split="test", trust_remote_code=trust_remote_code, revision=old_revision
     ).to_pandas()
     new_df = datasets.load_dataset(
-        path, config, split="test", revision=new_revision
+        path, config, split="test", trust_remote_code=trust_remote_code, revision=new_revision
     ).to_pandas()
 
     # Outer join includes rows that are in either of the dataframes.
-    merged_df = pd.merge(
-        new_df, old_df, on="name", how="inner", suffixes=("_new", "_old")
-    )
-    merged_df = merged_df[
-        ["name", "prompt_new", "prompt_old", "tests_new", "tests_old"]
-    ]
+    merged_df = pd.merge(new_df, old_df, on="name", how="inner", suffixes=('_new', '_old'))
+    merged_df = merged_df[["name", "prompt_new", "prompt_old", "tests_new", "tests_old"]]
     merged_df = merged_df.copy()
     merged_df["split"] = config
-    merged_df = merged_df[
-        (merged_df["prompt_new"] != merged_df["prompt_old"])
-        | (merged_df["tests_new"] != merged_df["tests_old"])
-    ]
+    merged_df = merged_df[(merged_df['prompt_new'] != merged_df['prompt_old']) | (merged_df['tests_new'] != merged_df['tests_old'])]
     return merged_df
 
 
-def main_with_args(path: str, old_revision: str, new_revision: str, output_dir: Path):
+def main_with_args(
+    path: str, old_revision: str, new_revision: str, trust_remote_code: bool,
+    output_dir: Path
+):
     common_splits = find_common_configs_and_report_added_and_removed(
-        path, old_revision, new_revision
+        path, old_revision, new_revision, trust_remote_code
     )
-    diffs = []
+    diffs = [ ]
     for split in tqdm(common_splits, desc="Common splits"):
-        diff = compare_configs_revisions(path, split, old_revision, new_revision)
+        diff = compare_configs_revisions(path, split, old_revision, new_revision, trust_remote_code)
         diffs.append(diff)
     diffs_df = pd.concat(diffs)
     diffs.clear()
@@ -119,8 +111,9 @@ def main():
         "--new_revision", type=str, help="New revision of the dataset", default="main"
     )
     parser.add_argument(
-        "--output-dir", type=Path, required=True, help="Path to output the diffs"
+        "--trust-remote-code", action="store_true", help="Trust remote code on the Hub"
     )
+    parser.add_argument("--output-dir", type=Path, required=True, help="Path to output the diffs")
     args = parser.parse_args()
     args_dict = vars(args)
     main_with_args(**args_dict)
